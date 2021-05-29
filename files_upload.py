110cdd03b411c2f1ee5294c03f820539349f361a
Mazhar <34889453+mazqoty@users.noreply.github.com>
2021-05-29 10:46:01 +0300

    Update files_upload.py

--- files_upload.py
+++ files_upload.py
@@ -1,28 +1,44 @@
 import streamlit as st
 import numpy as np
-#import pandas as pd
-from io import BytesIO
+import os
 from tensorflow.keras.preprocessing import image
 
 class FilesUpload(object):
 
     def __init__(self):
-        self.fileTypes = ["png", "jpg"]
+        self.fileTypes = ["png", "jpg", "jpeg"]
 
     def run(self):
+        
         image_shape = (130, 130, 3)
         st.set_option('deprecation.showfileUploaderEncoding', False)
-        img_file = st.file_uploader("Upload file", type=self.fileTypes)
+        img_file = st.file_uploader("Upload file", type = self.fileTypes)
         show_img_file = st.empty()
+        
         if not img_file:
-            show_img_file.info("Please upload a file of type: " + ", ".join(["png", "jpg"]))
+            show_img_file.info("Please upload a file of type: " + ", ".join(["png", "jpg", "jpeg"]))
             return
-        content = img_file.getvalue()
-        if isinstance(img_file, BytesIO):
+        
+        if img_file is not None:
+            
             st.image(img_file, width = 130) #use_column_width=True
-            img_file = image.load_img(img_file, target_size=image_shape)
+            file_details = {"File Name" : img_file.name,
+                            "File Type" : img_file.type,
+                            "File Size" : img_file.size
+                            }
+            st.write(file_details)
+            
+            with open(os.path.join("./temp/", img_file.name), 'wb') as f:
+                f.write(img_file.getbuffer())
+                file_path = "./temp/" + img_file.name
+                st.write(file_path)
+                #st.write("./temp/" + img_file.name)
+            
+            
+            img_file = image.load_img(file_path, grayscale = False, color_mode = 'rgb', target_size = image_shape, interpolation='nearest')
+            #img_file = image.load_img(img_file, target_size = image_shape)
             img_file = image.img_to_array(img_file)
-            img_file = np.expand_dims(img_file, axis=0)
+            img_file = np.expand_dims(img_file, axis = 0)
             img_file = img_file/255 #Normalizing the Image
             #st.text(img_file)
             st.text(img_file.shape)
@@ -31,4 +47,5 @@ class FilesUpload(object):
             return img_file
         else:
             st.write("Incorrect file or file extension")
+
 
